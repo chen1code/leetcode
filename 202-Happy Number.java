2c488ef1a3f50dc82018058386ec921f1f114b75
wangchen <35281799+chen1code@users.noreply.github.com>
2019-10-27 10:40:40 +0800

    Update and rename 202. Happy Number to 202-Happy Number.java

--- 202-Happy Number.java
+++ 202-Happy Number.java
@@ -1,3 +1,4 @@
+/*
 Write an algorithm to determine if a number is "happy".
 
 A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
@@ -11,10 +12,14 @@ Explanation:
 82 + 22 = 68
 62 + 82 = 100
 12 + 02 + 02 = 1
+*/
 
+"""
 解法：如果出现4，就返回false。可以尝试证明，为什么当且仅当出现4就不会出现1
 
-1. java
+Solution -- java
+"""
+
 class Solution {
     public boolean isHappy(int n) {
         if (n == 1) return true;
