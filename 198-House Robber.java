6cfb3b08fd68a264a92b249f1003a04087150bc0
wangchen <35281799+chen1code@users.noreply.github.com>
2019-10-27 10:39:31 +0800

    Update and rename 198. House Robber to 198-House Robber.java

--- 198-House Robber.java
+++ 198-House Robber.java
@@ -1,3 +1,4 @@
+/*
 You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
 
 Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
@@ -14,8 +15,9 @@ Input: [2,7,9,3,1]
 Output: 12
 Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
              Total amount you can rob = 2 + 9 + 1 = 12.
+*/
 
-
+"""
 Tips: 开始以为按奇偶分开求和取最大值 --- 错误理解
 
 解法步骤
@@ -24,8 +26,12 @@ Tips: 开始以为按奇偶分开求和取最大值 --- 错误理解
 2) ======= [2, 7, 11, , ] 2 + 9 > 7  第三位填较大值11
 3) ======= [2, 7, 11, 11, ] 3 + 7 < 11 第四位填较大值11
 4）======= [2, 7, 11, 11, 12] 11 + 1 > 11 第五位填12
+"""
 
-1. java
+"""
+Solution -- java
+"""
+
 class Solution {
     public int rob(int[] nums) {
         if(nums.length == 0)
