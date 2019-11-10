26fe8e73550ad248721614507afd5b77e2f0c1a4
wangchen <35281799+chen1code@users.noreply.github.com>
2019-11-10 14:56:06 +0800

    Create 345-Reverse Vowels of a String.java

---
+++ 345-Reverse Vowels of a String.java
@@ -0,0 +1,48 @@
+/*
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+
+Input: "hello"
+Output: "holle"
+Example 2:
+
+Input: "leetcode"
+Output: "leotcede"
+*/
+
+
+"""
+设立首位两个哨兵，遇到元音时记录下位置，并交换
+
+Solution -- Java
+
+"""
+
+class Solution {
+    public String reverseVowels(String s) {
+        if(s == null || s.length()==0) return s;
+        String vowels = "aeiouAEIOU";
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = s.length()-1;
+        while(start<end){
+
+            while(start<end && !vowels.contains(chars[start]+"")){
+                start++;
+            }
+
+            while(start<end && !vowels.contains(chars[end]+"")){
+                end--;
+            }
+
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+
+            start++;
+            end--;
+        }
+        return new String(chars);
+    }
+}
