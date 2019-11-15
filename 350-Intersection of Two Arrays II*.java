d079e7a411af7bb2bb7004aaa43413389c3342af
wangchen <35281799+chen1code@users.noreply.github.com>
2019-11-15 09:49:37 +0800

    Create 350-Intersection of Two Arrays II*.java

---
+++ 350-Intersection of Two Arrays II*.java
@@ -0,0 +1,46 @@
+/*
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+*/
+
+"""
+Solution - Java
+"""
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int pnt1 = 0;
+        int pnt2 = 0;
+        ArrayList<Integer> myList = new ArrayList<Integer>();
+        while((pnt1 < nums1.length) &&(pnt2< nums2.length)){
+            if(nums1[pnt1]<nums2[pnt2]){
+                pnt1++;
+            }
+            else{
+                if(nums1[pnt1]>nums2[pnt2]){
+                    pnt2++;
+                }
+                else{
+                    myList.add(nums1[pnt1]);
+                    pnt1++;
+                    pnt2++;
+                }
+            }
+        }
+        int[] res = new int[myList.size()];
+        for(int i = 0; i<res.length; i++){
+            res[i] = (Integer)myList.get(i);
+        }
+        return res;
+    }
+}
